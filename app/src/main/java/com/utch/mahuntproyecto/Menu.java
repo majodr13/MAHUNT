cb7bb98441a7d4895ec11cfb801864121b8ad650
EmmanuelRDGZ98 <emmanuel.rdgz98@gmail.com>
2024-11-14 17:33:41 -0600

    Video 6 terminado Cerrar Sesion

--- app/src/main/java/com/utch/mahuntproyecto/Menu.java
+++ app/src/main/java/com/utch/mahuntproyecto/Menu.java
@@ -2,6 +2,8 @@ package com.utch.mahuntproyecto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +20,8 @@ public class Menu extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
 
+    Button CerrarSesion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,14 @@ public class Menu extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance(); //Cambios video 5
         user = auth.getCurrentUser(); //Cambios video 5
+        CerrarSesion= findViewById(R.id.CerrarSesion); //Cambio video 6
+
+        CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CerrarSesion();
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -50,5 +62,11 @@ public class Menu extends AppCompatActivity {
             startActivity(new Intent(Menu.this, MainActivity.class));
             finish();
         }
+    }
+//Metodo cerrar cesion video 6
+    private  void CerrarSesion(){
+        auth.signOut();
+        startActivity(new Intent(Menu.this,MainActivity.class));
+        Toast.makeText(this, "Cerrado de sesion exitosamente", Toast.LENGTH_SHORT).show();
     }
 }
