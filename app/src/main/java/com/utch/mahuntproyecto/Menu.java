6e88bde39ca72b1e6821638d410d23b2970ec6de
EmmanuelRDGZ98 <emmanuel.rdgz98@gmail.com>
2024-11-19 16:20:57 -0600

    Cambio del metodo de consulta

--- app/src/main/java/com/utch/mahuntproyecto/Menu.java
+++ app/src/main/java/com/utch/mahuntproyecto/Menu.java
@@ -16,6 +16,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,13 +26,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Menu extends AppCompatActivity {
 
     FirebaseAuth auth;
     FirebaseUser user;
     FirebaseDatabase firebaseDatabase; //Video 9
-    DatabaseReference mahunt; // Video 9
+    DatabaseReference Mahunt; // Video 9
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     TextView MiPuntuaciontxt, uid, correo, nombre, Menutxt, Patos, fecha;
     Button CerrarSesion, JugarBtn, PuntuacionesBtn, AcercaDeBtn;
@@ -50,7 +57,7 @@ public class Menu extends AppCompatActivity {
         Typeface Tf = Typeface.createFromAsset(Menu.this.getAssets(), ubicacion);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
-        mahunt = firebaseDatabase.getReference("mahunt");
+        Mahunt = firebaseDatabase.getReference("mahunt");
 
 
         MiPuntuaciontxt= findViewById(R.id.MiPuntuaciontxt);
@@ -152,31 +159,42 @@ public class Menu extends AppCompatActivity {
     }
 
     //Consulta video 9
-    private  void Consulta(){
-        //Consulta compara con el Email
-        Query query= mahunt.orderByChild("Email").equalTo(user.getEmail());
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
-                for (DataSnapshot ds : datasnapshot.getChildren()){
-                    String patosString = ""+ds.child("Patos").getValue();
-                    String uidString = ""+ds.child("Uid").getValue();
-                    String emailString = ""+ ds.child("Email").getValue();
-                    String nombreString = ""+ds.child("Nombres").getValue();
-                    String fechaString = ""+ds.child("Fecha").getValue();
+    private void Consulta() {
 
-                    Patos.setText(patosString);
-                    uid.setText(uidString);
-                    correo.setText(emailString);
-                    nombre.setText(nombreString);
-                    fecha.setText(fechaString);
-                }
-            }
+        // Accede a la colección "mahunt" y busca el documento por el correo del usuario
+        db.collection("mahunt")
+                .whereEqualTo("Email", user.getEmail()) // Filtra por correo electrónico
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (!task.getResult().isEmpty()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    // Extrae los datos
+                                    String patosString = document.getString("Patos");
+                                    String uidString = document.getString("Uid");
+                                    String emailString = document.getString("Email");
+                                    String nombreString = document.getString("Nombres");
+                                    String fechaString = document.getString("Fecha");
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                                    // Muestra los datos en los TextView
+                                    Patos.setText(patosString);
+                                    uid.setText(uidString);
+                                    correo.setText(emailString);
+                                    nombre.setText(nombreString);
+                                    fecha.setText(fechaString);
 
-            }
-        });
+                                    Toast.makeText(Menu.this, "Datos cargados correctamente", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(Menu.this, "No se encontraron datos para el usuario", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(Menu.this, "Error al consultar datos: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 }
